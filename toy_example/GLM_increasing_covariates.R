b01155226a71c91c80088c39572399014c977b16
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-08-26 18:19:59 +0300

    Update GLM_increasing_covariates.R

--- toy_example/GLM_increasing_covariates.R
+++ toy_example/GLM_increasing_covariates.R
@@ -7,9 +7,9 @@ library(ggplot2)
 library(bridgesampling)
 set.seed(1)
 n <- 100
-results <- data.frame(k=integer(), logml_reshuffling = list(), pareto_k_numi_reshuffling = list(), pareto_k_deni_reshuffling = list(), mcse_logml_reshuffling = list(), logml_brute = list(), pareto_k_numi_brute = list(), pareto_k_deni_brute = list(), mcse_logml_brute = list())
-k <- 10
-for (k in 10:103) {
+results <- data.frame(k=integer(), logml_reshuffling = list(), pareto_k_numi_reshuffling = list(), pareto_k_deni_reshuffling = list(), mcse_logml_reshuffling = list(), logml_brute = list(), pareto_k_numi_brute = list(), pareto_k_deni_brute = list(), mcse_logml_brute = list(), numi_split = list(), deni_split = list(), numi_brute = list(), deni_brute = list())
+for (k in 10:60) {
+  print(paste("Number of covariates", k, "started"))
   set.seed(k)
   # Generate data
   X <- rnorm(n * k, mean=0, sd=1)
@@ -21,16 +21,21 @@ for (k in 10:103) {
   # Fit model
   set.seed(k)
   samples <- stan_glm(y ~ X, data=df, mean_PPD=FALSE, prior=hs(), seed=1, cores=parallel::detectCores(), diagnostic_file=file.path(tempdir(), "df2.csv"))
-  results_mine <- bridge_sampler(samples, method = "normal", num_splits = 6, total_perms = 100, return_always = TRUE, pareto_smoothing_all = TRUE, cores = parallel::detectCores())
+  results_mine <- bridge_sampler(samples, method = "normal", num_splits = 6, total_perms = 100, return_always = TRUE, cores = parallel::detectCores())
   logml_reshuffle <- lapply(results_mine, function(x) x$logml)
   pareto_k_numi_reshuffle <- lapply(results_mine, function(x) x$pareto_k_numi[[1]]$khat)
   pareto_k_deni_reshuffle <- lapply(results_mine, function(x) x$pareto_k_numi[[1]]$khat)
   mcse_logml_reshuffling <- lapply(results_mine, function(x) x$mcse_logml)
+  numi_split <- lapply(results_mine, function(x) x$numi)
+  deni_split <- lapply(results_mine, function(x) x$deni)
 
   logml_old <- numeric(100)
   pareto_k_numi_old <- numeric(100)
   pareto_k_deni_old <- numeric(100)
   mcse_logml_old <- numeric(100)
+  numi_brute <- numeric(100)
+  deni_brute <- numeric(100)
+
   for (i in 1:100) {
     set.seed(i)
     results_old <- bridge_sampler(samples, method = "normal", cores=parallel::detectCores())
@@ -38,6 +43,8 @@ for (k in 10:103) {
     pareto_k_numi_old[[i]] <- results_old$pareto_k_numi[[1]]$khat
     pareto_k_deni_old[[i]] <- results_old$pareto_k_deni[[1]]$khat
     mcse_logml_old[[i]] <- results_old$mcse_logml
+    numi_brute[[i]] <- results_old$numi
+    deni_brute[[i]] <- results_old$deni
 
     if(i>1){
       set.seed(k)
@@ -54,7 +61,11 @@ for (k in 10:103) {
     logml_brute = I(list(logml_old)),
     pareto_k_numi_brute = I(list(pareto_k_numi_old)),
     pareto_k_deni_brute = I(list(pareto_k_deni_old)),
-    mcse_logml_brute = I(list(mcse_logml_old))
+    mcse_logml_brute = I(list(mcse_logml_old)),
+    numi_split = I(list(numi_split)),
+    deni_split = I(list(deni_split)),
+    numi_brute = I(list(numi_brute)),
+    deni_brute = I(list(deni_brute))
   )
   results <- rbind(results, new_row)
   print("Saving progress")
