e2645e12bed23dfb3aa2e3ab4c987cd9b8521950
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-08-30 17:11:20 +0300

    Update nes.R

--- posteriordb/nes.R
+++ posteriordb/nes.R
@@ -1,5 +1,3 @@
-cmdstanr::cmdstan_make_local(cpp_options=list(STAN_THREADS=TRUE),append=TRUE)
-cmdstanr::rebuild_cmdstan()
 setwd("../posteriordb/")
 library(rstan)
 library(parallel)
@@ -22,21 +20,8 @@ sc <- stan_code(po)
 data <- get_data(po)
 model <- stan_model(model_code = sc)
 write_stan_file(sc, dir = getwd(), basename = "nes2000.stan")
-model_cmdstanr <- cmdstan_model("nes2000.stan", force_recompile = TRUE)
-init_val <- model_cmdstanr$pathfinder(data = data, 
-                                      num_paths = 10, 
-                                      single_path_draws = 40, 
-                                      draws = 400, 
-                                      history_size = 50, 
-                                      max_lbfgs_iters = 100, 
-                                      psis_resample = FALSE)
-fit_stan <- model_cmdstanr$sample(data = data,
-                                  chains = 4, 
-                                  iter_warmup = 1000, 
-                                  iter_sampling = 4000, 
-                                  thin = 1, 
-                                  init = init_val,
-                                  seed = 1)
+fit_stan <- stan(file = "nes2000.stan", data = data, 
+                 chains = 4, warmup = 1000, iter = 5000, thin = 1, seed = 1)
 print("Finished fitting the model")
 res <- bridge_sampler(fit_stan, num_splits = 6, total_perms = 100, seed = 1, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())
 results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
@@ -52,20 +37,8 @@ write.csv(results, file = "nes2000_pathfinder.csv", row.names = FALSE)
 results_bruteforce <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
 for (i in 1:100) {
   print(paste("Iteration", i))
-  init_val <- model_cmdstanr$pathfinder(data = data, 
-                                        num_paths = 10, 
-                                        single_path_draws = 40, 
-                                        draws = 400, 
-                                        history_size = 50, 
-                                        max_lbfgs_iters = 100, 
-                                        psis_resample = FALSE)
-  fit_stan <- model_cmdstanr$sample(data = data,
-                                    chains = 4, 
-                                    iter_warmup = 1000, 
-                                    iter_sampling = 4000, 
-                                    thin = 1, 
-                                    init = init_val,
-                                    seed = i)
+  fit_stan <- stan(file = "nes2000.stan", data = data, 
+                 chains = 4, warmup = 1000, iter = 5000, thin = 1, seed = i)
   res <- bridge_sampler(fit_stan, seed = i, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())
   results_bruteforce <- rbind(results_bruteforce, data.frame(logml = res$logml, 
                                        pareto_k_numi = res$pareto_k_numi, 
