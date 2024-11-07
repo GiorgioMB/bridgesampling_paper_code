16c8000ff07e107225d86e98c7c755a1a83b99aa
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-11-07 23:51:31 +0100

    Update bball.R

--- posteriordb/bball.R
+++ posteriordb/bball.R
@@ -1,3 +1,8 @@
+##
+remotes::install_github("stan-dev/cmdstanr@bridge_sampler-method")
+cmdstanr::cmdstan_make_local(cpp_options=list(STAN_THREADS=TRUE),append=TRUE)
+cmdstanr::rebuild_cmdstan()
+Sys.setenv(GITHUB_PAT = "YOUR_TOKEN")
 setwd("../posteriordb/")
 library(rstan)
 library(parallel)
@@ -7,8 +12,6 @@ library(bridgesampling)
 rstan_options(auto_write = TRUE)
 library(cmdstanr)
 library(bayesplot)
-## install the beta release version of R package posterior
-# install.packages("posterior", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 library(posteriordb)
 library(posterior)
 source("./utils/sim_pf.R")
@@ -20,12 +23,34 @@ sc <- stan_code(po)
 data <- get_data(po)
 model <- stan_model(model_code = sc)
 write_stan_file(sc, dir = getwd(), basename = "bball.stan")
-## RStan model fit
-fit_stan <- stan(file = "bball.stan", data = data, 
-                 chains = 4, warmup = 1000, iter = 6000, thin = 1, seed = 1)
-
-print("Finished fitting the model")
-res <- bridge_sampler(fit_stan, num_splits = 6, total_perms = 100, seed = 1, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())[[1]]
+model_cmdstanr <- cmdstan_model("bball.stan", force_recompile = TRUE)
+init_fun <- function() {
+  list(
+    theta1 = c(0.5, 0.5),
+    theta2 = c(0.5, 0.5),
+    phi = c(0, 3),
+    lambda = c(0, 3)
+  )
+}
+iteration_successful <- FALSE
+while(!iteration_successful){
+  tryCatch({
+    fit_stan <- model_cmdstanr$sample(data = data,
+                                      chains = 4, 
+                                      iter_warmup = 1000, 
+                                      iter_sampling = 4000, 
+                                      thin = 1, 
+                                      init = init_fun,
+                                      sig_figs=9
+                                      )
+    print("Finished fitting the model")
+    res <- bridge_sampler(fit_stan, num_splits = 6, total_perms = 100, seed = 1, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())[[1]]
+    iteration_successful <- TRUE
+  }, error = function(e) {
+    print("Iteration failed, retrying")
+  })
+}
+print("Iteration successful")
 results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
 for (j in 1:length(res)) {
   results <- rbind(results, data.frame(logml = res[[j]]$logml, 
@@ -34,7 +59,6 @@ for (j in 1:length(res)) {
                                        mcse_logml = res[[j]]$mcse_logml
                                        ))
 }
-
 write.csv(results, file = "bball_pathfinder.csv", row.names = FALSE)
 res <- bridge_sampler(fit_stan, num_splits = 6, total_perms = 100, seed = 1, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores(), pareto_smoothing_all = TRUE)[[1]]
 results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
@@ -45,14 +69,26 @@ for (j in 1:length(res)) {
                                        mcse_logml = res[[j]]$mcse_logml
                                        ))
 }
-
 write.csv(results, file = "bball_pathfinder_smoothed.csv", row.names = FALSE)
 results_bruteforce <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
 for (i in 1:100) {
   print(paste("Iteration", i))
-  fit_stan <- stan(file = "bball.stan", data = data, 
-                   chains = 4, warmup = 1000, iter = 6000, thin = 1, seed = i)
-  res <- bridge_sampler(fit_stan, seed = i, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())
+  iteration_successful <- FALSE
+  while(!iteration_successful){
+    tryCatch({
+      fit_stan <- model_cmdstanr$sample(data = data,
+                                        chains = 4, 
+                                        iter_warmup = 1000, 
+                                        iter_sampling = 4000, 
+                                        thin = 1, 
+                                        sig_figs=9
+                                        )
+      res <- bridge_sampler(fit_stan, seed = i, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())
+      iteration_successful <- TRUE
+    }, error = function(e) {
+      print("Iteration failed, retrying")
+    })
+  }
   results_bruteforce <- rbind(results_bruteforce, data.frame(logml = res$logml, 
                                        pareto_k_numi = res$pareto_k_numi, 
                                        pareto_k_deni = res$pareto_k_deni,
@@ -63,9 +99,22 @@ write.csv(results_bruteforce, file = "bball_pathfinder_bruteforce.csv", row.name
 results_bruteforce <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
 for (i in 1:100) {
   print(paste("Iteration", i))
-  fit_stan <- stan(file = "bball.stan", data = data, 
-                   chains = 4, warmup = 1000, iter = 6000, thin = 1, seed = i)
-  res <- bridge_sampler(fit_stan, seed = i, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores(), pareto_smoothing_all = TRUE)
+  iteration_successful <- FALSE
+  while(!iteration_successful){
+    tryCatch({
+      fit_stan <- model_cmdstanr$sample(data = data,
+                                        chains = 4, 
+                                        iter_warmup = 1000, 
+                                        iter_sampling = 4000, 
+                                        thin = 1, 
+                                        sig_figs=9
+                                        )
+      res <- bridge_sampler(fit_stan, seed = i, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores(), pareto_smoothing_all = TRUE)
+      iteration_successful <- TRUE
+    }, error = function(e) {
+      print("Iteration failed, retrying")
+    })
+  }
   results_bruteforce <- rbind(results_bruteforce, data.frame(logml = res$logml, 
                                        pareto_k_numi = res$pareto_k_numi, 
                                        pareto_k_deni = res$pareto_k_deni,
