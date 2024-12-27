bb86ebb0209e7202e6a3bc21c6e0ea341893d980
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-12-27 18:08:37 +0100

    Update bball.R

--- posteriordb/bball.R
+++ posteriordb/bball.R
@@ -1,9 +1,9 @@
 ##Note: The bridgesampling version of CmdstanR must be installed, comment the line below if already installed
 remotes::install_github("stan-dev/cmdstanr@bridge_sampler-method")
+Sys.setenv(GITHUB_PAT = "YOUR_TOKEN")
 cmdstanr::cmdstan_make_local(cpp_options=list(STAN_THREADS=TRUE),append=TRUE)
 cmdstanr::rebuild_cmdstan()
-Sys.setenv(GITHUB_PAT = "YOUR_TOKEN")
-setwd("../posteriordb/")
+setwd("posteriordb")
 library(rstan)
 library(parallel)
 library(foreach)
@@ -24,6 +24,13 @@ data <- get_data(po)
 model <- stan_model(model_code = sc)
 write_stan_file(sc, dir = getwd(), basename = "bball.stan")
 model_cmdstanr <- cmdstan_model("bball.stan", force_recompile = TRUE)
+#init_val <- model_cmdstanr$pathfinder(data = data, 
+#                                      num_paths = 10, 
+#                                      single_path_draws = 40, 
+#                                      draws = 400, 
+#                                      history_size = 50, 
+#                                      max_lbfgs_iters = 100, 
+#                                      psis_resample = FALSE)
 init_fun <- function() {
   list(
     theta1 = c(0.5, 0.5),
@@ -51,28 +58,72 @@ while(!iteration_successful){
   })
 }
 print("Iteration successful")
-results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
+numi_split <- I(list(lapply(res, function(x) x$numi)))
+deni_split <- I(list(lapply(res, function(x) x$deni)))
+pareto_k_numi_10 <- extract_khat(numi_split, n_draws = 10)[[1]]
+pareto_k_deni_10 <- extract_khat(deni_split, n_draws = 10)[[1]]
+pareto_k_numi_20 <- extract_khat(numi_split, n_draws = 20)[[1]]
+pareto_k_deni_20 <- extract_khat(deni_split, n_draws = 20)[[1]]
+print(length(pareto_k_numi_10))
+print(length(pareto_k_deni_10))
+print(length(pareto_k_numi_20))
+print(length(pareto_k_deni_20))
+print(length(res))
+results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric(), pareto_k_numi10 = numeric(), pareto_k_deni10 = numeric(), pareto_k_numi20 = numeric(), pareto_k_deni20 = numeric())
 for (j in 1:length(res)) {
   results <- rbind(results, data.frame(logml = res[[j]]$logml, 
                                        pareto_k_numi = res[[j]]$pareto_k_numi, 
                                        pareto_k_deni = res[[j]]$pareto_k_deni,
-                                       mcse_logml = res[[j]]$mcse_logml
+                                       mcse_logml = res[[j]]$mcse_logml,
+                                        pareto_k_numi10 = pareto_k_numi_10[[j]],
+                                        pareto_k_deni10 = pareto_k_deni_10[[j]],
+                                        pareto_k_numi20 = pareto_k_numi_20[[j]],
+                                        pareto_k_deni20 = pareto_k_deni_20[[j]]
                                        ))
 }
 write.csv(results, file = "bball_pathfinder.csv", row.names = FALSE)
+
+
+
 res <- bridge_sampler(fit_stan, num_splits = 6, total_perms = 100, seed = 1, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores(), pareto_smoothing_all = TRUE)[[1]]
-results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
+numi_split <- I(list(lapply(res, function(x) x$numi)))
+deni_split <- I(list(lapply(res, function(x) x$deni)))
+pareto_k_numi_10 <- extract_khat(numi_split, n_draws = 10)[[1]]
+pareto_k_deni_10 <- extract_khat(deni_split, n_draws = 10)[[1]]
+pareto_k_numi_20 <- extract_khat(numi_split, n_draws = 20)[[1]]
+pareto_k_deni_20 <- extract_khat(deni_split, n_draws = 20)[[1]]
+print(length(pareto_k_numi_10))
+print(length(pareto_k_deni_10))
+print(length(pareto_k_numi_20))
+print(length(pareto_k_deni_20))
+print(length(res))
+results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric(), pareto_k_numi10 = numeric(), pareto_k_deni10 = numeric(), pareto_k_numi20 = numeric(), pareto_k_deni20 = numeric())
 for (j in 1:length(res)) {
   results <- rbind(results, data.frame(logml = res[[j]]$logml, 
                                        pareto_k_numi = res[[j]]$pareto_k_numi, 
                                        pareto_k_deni = res[[j]]$pareto_k_deni,
-                                       mcse_logml = res[[j]]$mcse_logml
+                                       mcse_logml = res[[j]]$mcse_logml,
+                                        pareto_k_numi10 = pareto_k_numi_10[[j]],
+                                        pareto_k_deni10 = pareto_k_deni_10[[j]],
+                                        pareto_k_numi20 = pareto_k_numi_20[[j]],
+                                        pareto_k_deni20 = pareto_k_deni_20[[j]]
                                        ))
 }
 write.csv(results, file = "bball_pathfinder_smoothed.csv", row.names = FALSE)
+
+
 results_bruteforce <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
+numi <- numeric(100)
+deni <- numeric(100)
 for (i in 1:100) {
   print(paste("Iteration", i))
+  #init_val <- model_cmdstanr$pathfinder(data = data, 
+  #                                      num_paths = 10, 
+  #                                      single_path_draws = 40, 
+  #                                      draws = 400, 
+  #                                      history_size = 50, 
+  #                                      max_lbfgs_iters = 100, 
+  #                                      psis_resample = FALSE)
   iteration_successful <- FALSE
   while(!iteration_successful){
     tryCatch({
@@ -82,6 +133,7 @@ for (i in 1:100) {
                                         iter_sampling = 4000, 
                                         thin = 1, 
                                         sig_figs=9
+                                        #init = init_val,
                                         )
       res <- bridge_sampler(fit_stan, seed = i, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())
       iteration_successful <- TRUE
@@ -89,16 +141,37 @@ for (i in 1:100) {
       print("Iteration failed, retrying")
     })
   }
+  numi[[i]] <- res$numi
+  deni[[i]] <- res$deni
   results_bruteforce <- rbind(results_bruteforce, data.frame(logml = res$logml, 
                                        pareto_k_numi = res$pareto_k_numi, 
                                        pareto_k_deni = res$pareto_k_deni,
                                        mcse_logml = res$mcse_logml
                                        ))
 }
+numi <- I(list(numi))
+deni <- I(list(deni))
+pareto_k_numi_10_brute <- extract_khat(numi, n_draws = 10)[[1]]
+pareto_k_deni_10_brute <- extract_khat(deni, n_draws = 10)[[1]]
+pareto_k_numi_20_brute <- extract_khat(numi, n_draws = 20)[[1]]
+pareto_k_deni_20_brute <- extract_khat(deni, n_draws = 20)[[1]]
+results_bruteforce <- cbind(results_bruteforce, pareto_k_numi_10_brute, pareto_k_deni_10_brute, pareto_k_numi_20_brute, pareto_k_deni_20_brute)
 write.csv(results_bruteforce, file = "bball_pathfinder_bruteforce.csv", row.names = FALSE)
+
+
+
 results_bruteforce <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
+numi <- numeric(100)
+deni <- numeric(100)
 for (i in 1:100) {
   print(paste("Iteration", i))
+  #init_val <- model_cmdstanr$pathfinder(data = data, 
+  #                                      num_paths = 10, 
+  #                                      single_path_draws = 40, 
+  #                                      draws = 400, 
+  #                                      history_size = 50, 
+  #                                      max_lbfgs_iters = 100, 
+  #                                      psis_resample = FALSE)
   iteration_successful <- FALSE
   while(!iteration_successful){
     tryCatch({
@@ -108,6 +181,7 @@ for (i in 1:100) {
                                         iter_sampling = 4000, 
                                         thin = 1, 
                                         sig_figs=9
+                                        #init = init_val,
                                         )
       res <- bridge_sampler(fit_stan, seed = i, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores(), pareto_smoothing_all = TRUE)
       iteration_successful <- TRUE
@@ -115,10 +189,19 @@ for (i in 1:100) {
       print("Iteration failed, retrying")
     })
   }
+  numi[[i]] <- res$numi
+  deni[[i]] <- res$deni
   results_bruteforce <- rbind(results_bruteforce, data.frame(logml = res$logml, 
                                        pareto_k_numi = res$pareto_k_numi, 
                                        pareto_k_deni = res$pareto_k_deni,
                                        mcse_logml = res$mcse_logml
                                        ))
 }
+numi <- I(list(numi))
+deni <- I(list(deni))
+pareto_k_numi_10_brute <- extract_khat(numi, n_draws = 10)[[1]]
+pareto_k_deni_10_brute <- extract_khat(deni, n_draws = 10)[[1]]
+pareto_k_numi_20_brute <- extract_khat(numi, n_draws = 20)[[1]]
+pareto_k_deni_20_brute <- extract_khat(deni, n_draws = 20)[[1]]
+results_bruteforce <- cbind(results_bruteforce, pareto_k_numi_10_brute, pareto_k_deni_10_brute, pareto_k_numi_20_brute, pareto_k_deni_20_brute)
 write.csv(results_bruteforce, file = "bball_pathfinder_bruteforce_smoothed.csv", row.names = FALSE)
