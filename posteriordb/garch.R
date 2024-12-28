7b8ae531f861dc76d33a012af3b74c9b54c5b1d1
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-12-28 22:09:24 +0100

    Update garch.R

--- posteriordb/garch.R
+++ posteriordb/garch.R
@@ -1,9 +1,9 @@
 ##Note: The bridgesampling version of CmdstanR must be installed, comment the line below if already installed
 remotes::install_github("stan-dev/cmdstanr@bridge_sampler-method")
+Sys.setenv(GITHUB_PAT = "YOUR_TOKEN")
 cmdstanr::cmdstan_make_local(cpp_options=list(STAN_THREADS=TRUE),append=TRUE)
 cmdstanr::rebuild_cmdstan()
-Sys.setenv(GITHUB_PAT = "YOUR_TOKEN")
-setwd("../posteriordb/")
+setwd("posteriordb")
 library(rstan)
 library(parallel)
 library(foreach)
@@ -45,17 +45,38 @@ print("Finished fitting the model")
 res <- bridge_sampler(fit_stan, num_splits = 6, total_perms = 300, seed = 1, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())
 split_1 <- res[[1]]
 split_2 <- res[[2]]
-results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric(), split = list())
-for (j in 1:length(split_1)) {
-  results <- rbind(results, data.frame(logml = split_1[[j]]$logml, 
-                                       pareto_k_numi = split_1[[j]]$pareto_k_numi, 
-                                       pareto_k_deni = split_1[[j]]$pareto_k_deni,
-                                       mcse_logml = split_1[[j]]$mcse_logml
+res <- split_1
+numi_split <- I(list(lapply(res, function(x) x$numi)))
+deni_split <- I(list(lapply(res, function(x) x$deni)))
+pareto_k_numi_10 <- extract_khat(numi_split, n_draws = 10)[[1]]
+pareto_k_deni_10 <- extract_khat(deni_split, n_draws = 10)[[1]]
+pareto_k_numi_20 <- extract_khat(numi_split, n_draws = 20)[[1]]
+pareto_k_deni_20 <- extract_khat(deni_split, n_draws = 20)[[1]]
+print(length(pareto_k_numi_10))
+print(length(pareto_k_deni_10))
+print(length(pareto_k_numi_20))
+print(length(pareto_k_deni_20))
+print(length(res))
+results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric(), pareto_k_numi10 = numeric(), pareto_k_deni10 = numeric(), pareto_k_numi20 = numeric(), pareto_k_deni20 = numeric())
+for (j in 1:length(res)) {
+  results <- rbind(results, data.frame(logml = res[[j]]$logml, 
+                                       pareto_k_numi = res[[j]]$pareto_k_numi, 
+                                       pareto_k_deni = res[[j]]$pareto_k_deni,
+                                       mcse_logml = res[[j]]$mcse_logml,
+                                        pareto_k_numi10 = pareto_k_numi_10[[j]],
+                                        pareto_k_deni10 = pareto_k_deni_10[[j]],
+                                        pareto_k_numi20 = pareto_k_numi_20[[j]],
+                                        pareto_k_deni20 = pareto_k_deni_20[[j]]
                                        ))
 }
 write.csv(split_2, file = "garch_pathfinder_split2.csv", row.names = FALSE)
 write.csv(results, file = "garch_pathfinder.csv", row.names = FALSE)
+
+
+
 results_bruteforce <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
+numi <- numeric(300)
+deni <- numeric(300)
 for (i in 1:300) {
   iteration_complete <- FALSE
   while(!iteration_complete){
@@ -76,6 +97,8 @@ for (i in 1:300) {
                                           init = init_val,
                                           seed = i)
         res <- bridge_sampler(fit_stan, seed = i, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())
+        numi[[i]] <- res$numi
+        deni[[i]] <- res$deni
         results_bruteforce <- rbind(results_bruteforce, data.frame(logml = res$logml, 
                                             pareto_k_numi = res$pareto_k_numi, 
                                             pareto_k_deni = res$pareto_k_deni,
@@ -87,4 +110,11 @@ for (i in 1:300) {
     })
   }
 }
+numi <- I(list(numi))
+deni <- I(list(deni))
+pareto_k_numi_10_brute <- extract_khat(numi, n_draws = 10)[[1]]
+pareto_k_deni_10_brute <- extract_khat(deni, n_draws = 10)[[1]]
+pareto_k_numi_20_brute <- extract_khat(numi, n_draws = 20)[[1]]
+pareto_k_deni_20_brute <- extract_khat(deni, n_draws = 20)[[1]]
+results_bruteforce <- cbind(results_bruteforce, pareto_k_numi_10_brute, pareto_k_deni_10_brute, pareto_k_numi_20_brute, pareto_k_deni_20_brute)
 write.csv(results_bruteforce, file = "garch_pathfinder_bruteforce.csv", row.names = FALSE)
