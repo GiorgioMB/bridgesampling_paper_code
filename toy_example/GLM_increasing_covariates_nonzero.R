22ee6049be74b4af627ee9d994ddb535a434031a
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-12-29 16:47:34 +0100

    Update GLM_increasing_covariates_nonzero.R

--- toy_example/GLM_increasing_covariates_nonzero.R
+++ toy_example/GLM_increasing_covariates_nonzero.R
@@ -70,7 +70,7 @@ for (k in 10:104) {
   )
   results <- rbind(results, new_row)
   print("Saving progress")
-  saveRDS(results, "/scratch/work/micaleg1/bridge_sampling/glm_storing_everything_smaller_model.rds")
+  saveRDS(results, "glm_storing_everything_smaller_model.rds")
   print("Progress saved")
   print(paste("Number of covariates", k, "finished"))
 }
