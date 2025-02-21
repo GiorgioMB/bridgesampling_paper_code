d9dc53a01f656a4fdb1aa9d3561df7f0828c9b6a
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2025-02-21 15:09:30 -0800

    Update arma.R

--- posteriordb/arma.R
+++ posteriordb/arma.R
@@ -3,7 +3,7 @@ remotes::install_github("stan-dev/cmdstanr@bridge_sampler-method")
 Sys.setenv(GITHUB_PAT = "YOUR_TOKEN")
 cmdstanr::cmdstan_make_local(cpp_options=list(STAN_THREADS=TRUE),append=TRUE)
 cmdstanr::rebuild_cmdstan()
-setwd("posteriordb")
+setwd("../posteriordb")
 library(rstan)
 library(parallel)
 library(foreach)
@@ -39,7 +39,14 @@ fit_stan <- model_cmdstanr$sample(data = data,
                                   init = init_val,
                                   seed = 1)
 print("Finished fitting the model")
-res <- bridge_sampler(fit_stan, num_splits = 6, total_perms = 100, seed = 1, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())[[1]]
+res <- bridge_sampler(fit_stan, 
+                      num_splits = 6, 
+                      total_perms = 100, 
+                      seed = 1, 
+                      return_always = TRUE, 
+                      verbose = TRUE, 
+                      cores = parallel::detectCores(),
+                      calculate_covariance = TRUE)[[1]]
 numi_split <- I(list(lapply(res, function(x) x$numi)))
 deni_split <- I(list(lapply(res, function(x) x$deni)))
 pareto_k_numi_10 <- extract_khat(numi_split, n_draws = 10)[[1]]
@@ -66,8 +73,51 @@ for (j in 1:length(res)) {
 
 write.csv(results, file = "arma11_pathfinder.csv", row.names = FALSE)
 
+res <- bridge_sampler(fit_stan, 
+                      num_splits = 6, 
+                      total_perms = 100, 
+                      seed = 1, 
+                      return_always = TRUE, 
+                      verbose = TRUE, 
+                      cores = parallel::detectCores(),
+                      calculate_covariance = FALSE)[[1]]
+numi_split <- I(list(lapply(res, function(x) x$numi)))
+deni_split <- I(list(lapply(res, function(x) x$deni)))
+pareto_k_numi_10 <- extract_khat(numi_split, n_draws = 10)[[1]]
+pareto_k_deni_10 <- extract_khat(deni_split, n_draws = 10)[[1]]
+pareto_k_numi_20 <- extract_khat(numi_split, n_draws = 20)[[1]]
+pareto_k_deni_20 <- extract_khat(deni_split, n_draws = 20)[[1]]
+print(length(pareto_k_numi_10))
+print(length(pareto_k_deni_10))
+print(length(pareto_k_numi_20))
+print(length(pareto_k_deni_20))
+print(length(res))
+results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric(), pareto_k_numi10 = numeric(), pareto_k_deni10 = numeric(), pareto_k_numi20 = numeric(), pareto_k_deni20 = numeric())
+for (j in 1:length(res)) {
+  results <- rbind(results, data.frame(logml = res[[j]]$logml, 
+                                       pareto_k_numi = res[[j]]$pareto_k_numi, 
+                                       pareto_k_deni = res[[j]]$pareto_k_deni,
+                                       mcse_logml = res[[j]]$mcse_logml,
+                                        pareto_k_numi10 = pareto_k_numi_10[[j]],
+                                        pareto_k_deni10 = pareto_k_deni_10[[j]],
+                                        pareto_k_numi20 = pareto_k_numi_20[[j]],
+                                        pareto_k_deni20 = pareto_k_deni_20[[j]]
+                                       ))
+}
 
-res <- bridge_sampler(fit_stan, num_splits = 6, total_perms = 100, seed = 1, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores(), pareto_smoothing_all = TRUE)[[1]]
+write.csv(results, file = "arma11_pathfinder_no_cov.csv", row.names = FALSE)
+
+
+
+res <- bridge_sampler(fit_stan, 
+                      num_splits = 6, 
+                      total_perms = 100, 
+                      seed = 1, 
+                      return_always = TRUE, 
+                      verbose = TRUE, 
+                      cores = parallel::detectCores(), 
+                      pareto_smoothing_all = TRUE,
+                      calculate_covariance = TRUE)[[1]]
 numi_split <- I(list(lapply(res, function(x) x$numi)))
 deni_split <- I(list(lapply(res, function(x) x$deni)))
 pareto_k_numi_10 <- extract_khat(numi_split, n_draws = 10)[[1]]
@@ -94,6 +144,41 @@ for (j in 1:length(res)) {
 
 write.csv(results, file = "arma11_pathfinder_smoothed.csv", row.names = FALSE)
 
+res <- bridge_sampler(fit_stan, 
+                      num_splits = 6, 
+                      total_perms = 100, 
+                      seed = 1, 
+                      return_always = TRUE, 
+                      verbose = TRUE, 
+                      cores = parallel::detectCores(), 
+                      pareto_smoothing_all = TRUE,
+                      calculate_covariance = FALSE)[[1]]
+numi_split <- I(list(lapply(res, function(x) x$numi)))
+deni_split <- I(list(lapply(res, function(x) x$deni)))
+pareto_k_numi_10 <- extract_khat(numi_split, n_draws = 10)[[1]]
+pareto_k_deni_10 <- extract_khat(deni_split, n_draws = 10)[[1]]
+pareto_k_numi_20 <- extract_khat(numi_split, n_draws = 20)[[1]]
+pareto_k_deni_20 <- extract_khat(deni_split, n_draws = 20)[[1]]
+print(length(pareto_k_numi_10))
+print(length(pareto_k_deni_10))
+print(length(pareto_k_numi_20))
+print(length(pareto_k_deni_20))
+print(length(res))
+results <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric(), pareto_k_numi10 = numeric(), pareto_k_deni10 = numeric(), pareto_k_numi20 = numeric(), pareto_k_deni20 = numeric())
+for (j in 1:length(res)) {
+  results <- rbind(results, data.frame(logml = res[[j]]$logml, 
+                                       pareto_k_numi = res[[j]]$pareto_k_numi, 
+                                       pareto_k_deni = res[[j]]$pareto_k_deni,
+                                       mcse_logml = res[[j]]$mcse_logml,
+                                        pareto_k_numi10 = pareto_k_numi_10[[j]],
+                                        pareto_k_deni10 = pareto_k_deni_10[[j]],
+                                        pareto_k_numi20 = pareto_k_numi_20[[j]],
+                                        pareto_k_deni20 = pareto_k_deni_20[[j]]
+                                       ))
+}
+
+write.csv(results, file = "arma11_pathfinder_smoothed_no_cov.csv", row.names = FALSE)
+
 
 results_bruteforce <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
 numi <- numeric(100)
@@ -115,7 +200,12 @@ for (i in 1:100) {
                                     thin = 1, 
                                     init = init_val,
                                     seed = i)
-  res <- bridge_sampler(fit_stan, seed = i, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores())
+  res <- bridge_sampler(fit_stan, 
+                        seed = i, 
+                        return_always = TRUE, 
+                        verbose = TRUE, 
+                        cores = parallel::detectCores(),
+                        calculate_covariance = TRUE)
   numi[[i]] <- res$numi
   deni[[i]] <- res$deni
   results_bruteforce <- rbind(results_bruteforce, data.frame(logml = res$logml, 
@@ -154,7 +244,57 @@ for (i in 1:100) {
                                     thin = 1, 
                                     init = init_val,
                                     seed = i)
-  res <- bridge_sampler(fit_stan, seed = i, return_always = TRUE, verbose = TRUE, cores = parallel::detectCores(), pareto_smoothing_all = TRUE)
+  res <- bridge_sampler(fit_stan, 
+                        seed = i, 
+                        return_always = TRUE, 
+                        verbose = TRUE, 
+                        cores = parallel::detectCores(),
+                        calculate_covariance = FALSE)
+  numi[[i]] <- res$numi
+  deni[[i]] <- res$deni
+  results_bruteforce <- rbind(results_bruteforce, data.frame(logml = res$logml, 
+                                       pareto_k_numi = res$pareto_k_numi, 
+                                       pareto_k_deni = res$pareto_k_deni,
+                                       mcse_logml = res$mcse_logml
+                                       ))
+}
+numi <- I(list(numi))
+deni <- I(list(deni))
+pareto_k_numi_10_brute <- extract_khat(numi, n_draws = 10)[[1]]
+pareto_k_deni_10_brute <- extract_khat(deni, n_draws = 10)[[1]]
+pareto_k_numi_20_brute <- extract_khat(numi, n_draws = 20)[[1]]
+pareto_k_deni_20_brute <- extract_khat(deni, n_draws = 20)[[1]]
+results_bruteforce <- cbind(results_bruteforce, pareto_k_numi_10_brute, pareto_k_deni_10_brute, pareto_k_numi_20_brute, pareto_k_deni_20_brute)
+write.csv(results_bruteforce, file = "arma11_pathfinder_bruteforce_no_cov.csv", row.names = FALSE)
+
+
+results_bruteforce <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
+numi <- numeric(100)
+deni <- numeric(100)
+for (i in 1:100) {
+  print(paste("Iteration", i))
+  model_cmdstanr <- cmdstan_model("arma-arma11.stan", force_recompile = TRUE)
+  init_val <- model_cmdstanr$pathfinder(data = data, 
+                                        num_paths = 10, 
+                                        single_path_draws = 40, 
+                                        draws = 400, 
+                                        history_size = 50, 
+                                        max_lbfgs_iters = 100, 
+                                        psis_resample = FALSE)
+  fit_stan <- model_cmdstanr$sample(data = data,
+                                    chains = 4, 
+                                    iter_warmup = 1000, 
+                                    iter_sampling = 4000, 
+                                    thin = 1, 
+                                    init = init_val,
+                                    seed = i)
+  res <- bridge_sampler(fit_stan, 
+                        seed = i, 
+                        return_always = TRUE, 
+                        verbose = TRUE, 
+                        cores = parallel::detectCores(), 
+                        pareto_smoothing_all = TRUE,
+                        calculate_covariance = TRUE)
   numi[[i]] <- res$numi
   deni[[i]] <- res$deni
   results_bruteforce <- rbind(results_bruteforce, data.frame(logml = res$logml, 
@@ -171,3 +311,48 @@ pareto_k_numi_20_brute <- extract_khat(numi, n_draws = 20)[[1]]
 pareto_k_deni_20_brute <- extract_khat(deni, n_draws = 20)[[1]]
 results_bruteforce <- cbind(results_bruteforce, pareto_k_numi_10_brute, pareto_k_deni_10_brute, pareto_k_numi_20_brute, pareto_k_deni_20_brute)
 write.csv(results_bruteforce, file = "arma11_pathfinder_bruteforce_smoothed.csv", row.names = FALSE)
+
+
+results_bruteforce <- data.frame(logml = numeric(), pareto_k_numi = numeric(), pareto_k_deni = numeric(), mcse_logml = numeric())
+numi <- numeric(100)
+deni <- numeric(100)
+for (i in 1:100) {
+  print(paste("Iteration", i))
+  model_cmdstanr <- cmdstan_model("arma-arma11.stan", force_recompile = TRUE)
+  init_val <- model_cmdstanr$pathfinder(data = data, 
+                                        num_paths = 10, 
+                                        single_path_draws = 40, 
+                                        draws = 400, 
+                                        history_size = 50, 
+                                        max_lbfgs_iters = 100, 
+                                        psis_resample = FALSE)
+  fit_stan <- model_cmdstanr$sample(data = data,
+                                    chains = 4, 
+                                    iter_warmup = 1000, 
+                                    iter_sampling = 4000, 
+                                    thin = 1, 
+                                    init = init_val,
+                                    seed = i)
+  res <- bridge_sampler(fit_stan, 
+                        seed = i, 
+                        return_always = TRUE, 
+                        verbose = TRUE, 
+                        cores = parallel::detectCores(), 
+                        pareto_smoothing_all = TRUE,
+                        calculate_covariance = FALSE)
+  numi[[i]] <- res$numi
+  deni[[i]] <- res$deni
+  results_bruteforce <- rbind(results_bruteforce, data.frame(logml = res$logml, 
+                                       pareto_k_numi = res$pareto_k_numi, 
+                                       pareto_k_deni = res$pareto_k_deni,
+                                       mcse_logml = res$mcse_logml
+                                       ))
+}
+numi <- I(list(numi))
+deni <- I(list(deni))
+pareto_k_numi_10_brute <- extract_khat(numi, n_draws = 10)[[1]]
+pareto_k_deni_10_brute <- extract_khat(deni, n_draws = 10)[[1]]
+pareto_k_numi_20_brute <- extract_khat(numi, n_draws = 20)[[1]]
+pareto_k_deni_20_brute <- extract_khat(deni, n_draws = 20)[[1]]
+results_bruteforce <- cbind(results_bruteforce, pareto_k_numi_10_brute, pareto_k_deni_10_brute, pareto_k_numi_20_brute, pareto_k_deni_20_brute)
+write.csv(results_bruteforce, file = "arma11_pathfinder_bruteforce_smoothed_no_cov.csv", row.names = FALSE)
